18ed2a1d30ac926aa12559bcd1ffb73c8a2c5314
kobajs <kobashigawa.dev@gmail.com>
2021-02-20 22:22:18 -0300

    feat: finished MarketRateChart

--- src/features/MarketRateAnalytics/MarketRateChart/MarketRateChart.tsx
+++ src/features/MarketRateAnalytics/MarketRateChart/MarketRateChart.tsx
@@ -22,24 +22,29 @@ export const MarketRateChart: FC<MarketRateChartProps> = ({ label }) => {
     <div>
       <h1>My Chart</h1>
       <ResponsiveContainer>
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
+        <LineChart width={500} height={300} data={data}>
+          <CartesianGrid vertical={false} />
           <XAxis
             dataKey="day"
             tickFormatter={(value) => moment(value).format('MMM DD')}
+            interval={7}
           />
-          <YAxis tickFormatter={(value) => `$${value}`} />
-          <Tooltip />
+          <YAxis
+            type="number"
+            tickCount={8}
+            // Choose to not extract those functions since it's component coupled and brings a complete reading about what is doing.
+            tickFormatter={(value) => `$${value}`}
+            domain={[
+              (dataMin: number) => Math.floor(dataMin / 100) * 100 - 200,
+              (dataMax: number) => Math.floor(dataMax / 100) * 100 + 200,
+            ]}
+          />
+          <Tooltip
+            formatter={(value: number) => `$${value}`}
+            labelFormatter={(label: Date) =>
+              moment(label).format('MMM DD, YYYY')
+            }
+          />
           <Legend />
           <Line type="monotone" dataKey="low" stroke="#8884d8" />
           <Line type="monotone" dataKey="mean" stroke="#82ca9d" />
