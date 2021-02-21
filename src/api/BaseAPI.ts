f78977bbc01a81a3aff7919c8ff59ca64691752c
kobajs <kobashigawa.dev@gmail.com>
2021-02-21 00:35:05 -0300

    feat: adds BaseAPI

---
+++ src/api/BaseAPI.ts
@@ -0,0 +1,20 @@
+export class BaseAPI {
+  baseUrl: string
+  token: string
+
+  constructor() {
+    const API_URL = process.env.REACT_APP_API_URL
+    const API_TOKEN = process.env.REACT_APP_API_TOKEN
+
+    if (!API_URL) {
+      throw new Error('REACT_APP_API_URL environment is not defined!')
+    }
+
+    if (!API_TOKEN) {
+      throw new Error('REACT_APP_API_TOKEN environment is not defined!')
+    }
+
+    this.baseUrl = API_URL as string
+    this.token = API_TOKEN as string
+  }
+}
---
+++ src/api/__tests__/BaseAPI.test.ts
@@ -0,0 +1,40 @@
+import { BaseAPI } from '../BaseAPI'
+
+describe('BaseAPI', () => {
+  afterEach(() => {
+    delete process.env.REACT_APP_API_URL
+    delete process.env.REACT_APP_API_TOKEN
+  })
+
+  it('should have the environment variable REACT_APP_API_URL as baseUrl', async () => {
+    process.env.REACT_APP_API_URL = 'https://youtube.com'
+    process.env.REACT_APP_API_TOKEN = 'My Token'
+
+    const baseAPI = new BaseAPI()
+    expect(baseAPI.baseUrl).toBe('https://youtube.com')
+  })
+
+  it('should have the environment variable REACT_APP_API_TOKEN as token', async () => {
+    process.env.REACT_APP_API_URL = 'https://youtube.com'
+    process.env.REACT_APP_API_TOKEN = 'My Token'
+
+    const baseAPI = new BaseAPI()
+    expect(baseAPI.token).toBe('My Token')
+  })
+
+  it('should throw an error if REACT_APP_API_URL is undefined', async () => {
+    process.env.REACT_APP_API_TOKEN = 'My Token'
+
+    expect(() => {
+      new BaseAPI()
+    }).toThrowError('REACT_APP_API_URL environment is not defined!')
+  })
+
+  it('should throw an error if REACT_APP_API_TOKEN is undefined', async () => {
+    process.env.REACT_APP_API_URL = 'https://youtube.com'
+
+    expect(() => {
+      new BaseAPI()
+    }).toThrowError('REACT_APP_API_TOKEN environment is not defined!')
+  })
+})
