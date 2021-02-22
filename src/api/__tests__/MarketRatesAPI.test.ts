48e28f9c87b7a94f4867bc46170c2e8b52dcb0f9
Yuri Tavares Kobashigawa <kobashigawa.dev@gmail.com>
2021-02-22 09:30:03 -0300

    fix: mocks on APIs tests

--- src/api/__tests__/MarketRatesAPI.test.ts
+++ src/api/__tests__/MarketRatesAPI.test.ts
@@ -1,39 +1,51 @@
-import { AxiosRequestConfig } from 'axios'
-import { MarketRatesAPI } from '../MarketRatesAPI'
+import { AxiosRequestConfig } from "axios";
+import { MarketRatesAPI } from "../MarketRatesAPI";
 const mockInstanceGet = jest.fn(
   (path: string, options: AxiosRequestConfig) => ({ data: [] })
-)
+);
 
-jest.mock('axios', () => ({
+jest.mock("axios", () => ({
   create: jest.fn(() => ({
     get: mockInstanceGet,
   })),
-}))
+}));
 
-describe('MarketRatesAPI', () => {
-  it('should use instance get on getAllMarketRatesForPorts() using path as argument and origin/destination as parameters', async () => {
-    await new MarketRatesAPI().getAllMarketRatesForPorts('CNSGH', 'NLRTM')
-    expect(mockInstanceGet).toHaveBeenCalledWith('/rates', {
+const ApiUrl = process.env.REACT_APP_API_URL;
+const ApiToken = process.env.REACT_APP_API_TOKEN;
+
+describe("MarketRatesAPI", () => {
+  beforeAll(() => {
+    process.env.REACT_APP_API_URL = "https://youtube.com";
+    process.env.REACT_APP_API_TOKEN = "my-token";
+  });
+  afterAll(() => {
+    process.env.REACT_APP_API_URL = ApiUrl;
+    process.env.REACT_APP_API_TOKEN = ApiToken;
+  });
+
+  it("should use instance get on getAllMarketRatesForPorts() using path as argument and origin/destination as parameters", async () => {
+    await new MarketRatesAPI().getAllMarketRatesForPorts("CNSGH", "NLRTM");
+    expect(mockInstanceGet).toHaveBeenCalledWith("/rates", {
       params: {
-        origin: 'CNSGH',
-        destination: 'NLRTM',
+        origin: "CNSGH",
+        destination: "NLRTM",
       },
-    })
-  })
+    });
+  });
 
-  it('should throw error if origin has length different than 5 characters', async () => {
+  it("should throw error if origin has length different than 5 characters", async () => {
     await expect(
-      new MarketRatesAPI().getAllMarketRatesForPorts('CNS', 'NLRTM')
+      new MarketRatesAPI().getAllMarketRatesForPorts("CNS", "NLRTM")
     ).rejects.toEqual(
-      new Error('Origin needs to have 5 characters (Port Code)')
-    )
-  })
+      new Error("Origin needs to have 5 characters (Port Code)")
+    );
+  });
 
-  it('should throw error if destination has length different than 5 characters', async () => {
+  it("should throw error if destination has length different than 5 characters", async () => {
     await expect(
-      new MarketRatesAPI().getAllMarketRatesForPorts('CNSGH', 'NLR')
+      new MarketRatesAPI().getAllMarketRatesForPorts("CNSGH", "NLR")
     ).rejects.toEqual(
-      new Error('Origin needs to have 5 characters (Port Code)')
-    )
-  })
-})
+      new Error("Origin needs to have 5 characters (Port Code)")
+    );
+  });
+});
--- src/api/__tests__/PortsAPI.test.ts
+++ src/api/__tests__/PortsAPI.test.ts
@@ -1,15 +1,27 @@
-import { PortsAPI } from '../PortsAPI'
-const mockInstanceGet = jest.fn((path: string) => ({ data: [] }))
+import { PortsAPI } from "../PortsAPI";
+const mockInstanceGet = jest.fn((path: string) => ({ data: [] }));
 
-jest.mock('axios', () => ({
+jest.mock("axios", () => ({
   create: jest.fn(() => ({
     get: mockInstanceGet,
   })),
-}))
+}));
 
-describe('PortsAPI', () => {
-  it('should use instance get on getAllPorts() with path argurment', async () => {
-    await new PortsAPI().getAllPorts()
-    expect(mockInstanceGet).toHaveBeenCalledWith('/ports')
-  })
-})
+const ApiUrl = process.env.REACT_APP_API_URL;
+const ApiToken = process.env.REACT_APP_API_TOKEN;
+
+describe("PortsAPI", () => {
+  beforeAll(() => {
+    process.env.REACT_APP_API_URL = "https://youtube.com";
+    process.env.REACT_APP_API_TOKEN = "my-token";
+  });
+  afterAll(() => {
+    process.env.REACT_APP_API_URL = ApiUrl;
+    process.env.REACT_APP_API_TOKEN = ApiToken;
+  });
+
+  it("should use instance get on getAllPorts() with path argurment", async () => {
+    await new PortsAPI().getAllPorts();
+    expect(mockInstanceGet).toHaveBeenCalledWith("/ports");
+  });
+});
